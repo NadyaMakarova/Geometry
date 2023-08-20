2eff0baf3da27ee597796dc8e174c71481d88816
Naya Makarova <Nadecdamak.908@yandex.ru>
2023-08-21 00:24:22 +0300

    feat: создан проект

---
+++ GeometryMakarova/Views/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+
+namespace GeometryMakarova.Views
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+
+        }
+    }
+}
---
+++ ModelLibrary/Figure.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLibrary
+{
+    public class Figure
+    {
+        #region Properties
+
+        public Parameters Parameters { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        public Figure(Parameters parameters)
+        {
+            Parameters = parameters;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public virtual double? ReturnS()
+        {
+            if (Parameters != null && Parameters?.A != null && Parameters.H != null)
+                return Parameters?.A * Parameters?.H;
+            return null;
+        }
+
+        #endregion
+    }
+}
